707dc3d081e45fe6e39ec111d7f3eab0a6c37dc3
karthickag04 <karthickag04@gmail.com>
2024-01-04 16:06:17 +0530

    added javascript console demo scripts

---
+++ js/scriptjs.js
@@ -0,0 +1,71 @@
+//to use console check the below line
+console.log("Welcome to javascript");
+console.log('Welcome to javascript1');
+console.log(10);
+console.log(10.10);
+//to print objects in console
+console.log({fname:"karthick", gender:"Male"});
+console.table({fname:"karthick", gender:"Male"});
+//to print Array in console
+console.log([5,10,20,15]);
+console.table([5,10,20,15]);
+console.log(true);
+
+
+// print in console with variable declartion
+
+//var a;
+//let b;
+var a=5;
+let b=7;
+const c=10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=6;
+console.log(a);
+b=8;
+console.log(b);
+console.log(c);
+
+a=6.5;
+console.log(a);
+b=8.5;
+console.log(b);
+
+
+a="karthick";
+console.log(a);
+b="RAM";
+console.log(b);
+
+// you can redeclare variable used in "Var" method
+//var a=25;
+//console.log(a);
+// you cannot redeclare variable used in "let" method 
+//let b=25;
+//console.log(b);
+
+
+a={fname:"karthick", gender:"Male"};
+console.log(a);
+console.table(a);
+
+b={fname:"karthick", gender:"Male"};
+console.log(b);
+console.table(b);
+
+
+a=[12,11,25,60];
+console.log(a);
+console.table(a);
+
+
+b=[12,11,25,60];
+console.log(b);
+console.table(b);
+
+
+
