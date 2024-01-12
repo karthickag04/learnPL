5ef4e5cc24a8ccd2a25e8a5dded23af1f20d2f28
karthickag04 <karthickag04@gmail.com>
2024-01-12 15:55:20 +0530

    added files to javascript session 07 to learnpl

---
+++ JavaScript/session-07/js/scriptjs.js
@@ -0,0 +1,17 @@
+function RegisterForm(){
+
+    var firstname=document.getElementById("tbfname").value;
+    
+    document.getElementById("fname").innerHTML=firstname;
+    var Middlename=document.getElementById("tbmname").value;
+    
+    document.getElementById("mname").innerHTML=Middlename;
+
+    var Lastnmae=document.getElementById("tblname").value;
+    
+    document.getElementById("lname").innerHTML=Lastnmae;
+    var PhoneNo=document.getElementById("tbphone").value;
+    document.getElementById("phone").innerHTML=PhoneNo;
+
+
+}
