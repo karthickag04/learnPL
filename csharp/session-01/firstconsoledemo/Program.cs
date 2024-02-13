ddf74a4c09364bafb879e40ca2915370381d1408
karthickag04 <karthickag04@gmail.com>
2024-02-13 16:54:11 +0530

    added files to csharp session 01

---
+++ csharp/session-01/firstconsoledemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// int a=10;
+// Console.WriteLine(a);
+// Console.WriteLine("the given value for a is " + a);
+
+// // int , float , double, string, char, long, boolean, array
+// float b=10.5f;
+// Console.WriteLine(b);
+// Console.WriteLine("the given value for b is " + b);
+
+
+// Double c=10.5D;
+// Console.WriteLine(c);
+// Console.WriteLine("the given value for c is " + c);
+// Console.WriteLine(c.GetType());
+
+
+// char s='k';
+// Console.WriteLine(s);
+// Console.WriteLine("the given value for c is " + s);
+// Console.WriteLine(s.GetType());
+
+// string str="welcome to great karikalan magic show";
+// Console.WriteLine(str);
+// Console.WriteLine("the given value for c is " + str);
+// Console.WriteLine(str.GetType());
+
+
+int getvalue;
+getvalue=int.Parse(Console.ReadLine());
+Console.WriteLine(getvalue);
+Console.WriteLine("the given value through  console for getvalue  is " + getvalue);
