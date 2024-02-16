faf4faaa903cc925d46656826ce056ec6b70d6d2
karthickag04 <karthickag04@gmail.com>
2024-02-16 18:16:34 +0530

    added class session files to session 02

---
+++ csharp/session-02/program/Program.cs
@@ -0,0 +1,83 @@
+﻿// string a="Welcome to C# through variable";
+// Console.WriteLine(a);
+
+class Program{
+
+  static void Main(string [] args){
+    Console.WriteLine("Welcome To Classes");
+    test tt=new test();
+    tt.PrintMSG();
+    tt.PrintMSG2();
+
+  }
+}
+
+
+public class test{
+  string name="CAD BATCH 01";
+
+  public void PrintMSG(){
+    Console.WriteLine(name);
+  }
+
+  public void PrintMSG2(){
+    Console.WriteLine("hi ..! " + name);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Programming{
+//         static void Main(string [] args){
+//             Console.WriteLine("welcome to namespace demo12");
+//             test tt=new test();
+//             tt.print1();
+//         }
+
+
+//     }
+
+// public class test{
+//   string x="karthick";
+//   public void print1(){
+//     Console.WriteLine(x);
+//   }
+
+// }
