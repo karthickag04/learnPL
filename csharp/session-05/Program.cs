b0e20090a6c9cc60fcafeef006ca0ec8fba174b3
karthickag04 <karthickag04@gmail.com>
2024-02-20 18:06:58 +0530

    added single inheritance session files to session 06

---
+++ csharp/session-05/Program.cs
@@ -0,0 +1,44 @@
+﻿class Kalaingar{
+
+    public string location;
+    
+    public Kalaingar()
+    {
+        Console.WriteLine("Welcome to Kalaingar family");
+    }
+    public string partyname(string ppartyname){
+        
+        return "I have Political Party " + ppartyname ;
+
+    }
+
+
+}
+
+class mkstalin : Kalaingar
+{
+     public mkstalin(string name){
+        location = "gopalapuram";
+        Console.WriteLine(partyname(name) + " " + location);
+     }
+}
+
+
+class mkazhagiri : Kalaingar
+{
+     public mkazhagiri(string name){
+         location = "Madurai";
+         Console.WriteLine(partyname(name) + " " + location);
+     }
+}
+
+class family{
+
+    static void Main(string [] args){
+       // Kalaingar kr=new Kalaingar();
+       mkstalin stalin=new mkstalin("dmk");
+       mkazhagiri azhagiri=new mkazhagiri("dmk");
+        
+
+    }
+}
