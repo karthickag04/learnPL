4d9a173000c43f4757938654485a569389c052bd
karthickag04 <karthickag04@gmail.com>
2024-02-26 17:41:57 +0530

    added first mvc site demo in c#

---
+++ csharp/session-08/firstsitecsharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace firstsitecsharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
