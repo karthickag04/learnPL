8b69fad6a22f93188686c331dcf17c72499317eb
karthickag04 <karthickag04@gmail.com>
2024-02-26 18:09:21 +0530

    added files to interfacedemo repo under session 08

---
+++ csharp/session-08/interfacedemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+ public interface Drawable{
+       void draw();
+    }
+
+    
+ public interface SetDrawable{
+       void SetColour();
+    }
+
+    public class Circle : Drawable, SetDrawable
+    {
+        public void draw(){
+            Console.WriteLine("I can Draw circle");
+        }
+         public void SetColour(){
+            Console.WriteLine("I can set circle colour as black");
+        }
+
+        
+    }
+
+
+    
+    public class Rectangle : Drawable{
+        public void draw(){
+            Console.WriteLine("I can Draw Rectangle");
+        }
+    }
+
+        public class Square : Drawable{
+        public void draw(){
+            Console.WriteLine("I can Draw Square");
+        }
+    }
+
+         public class Line : Drawable{
+        public void draw(){
+            Console.WriteLine("I can Draw Line");
+        }
+    }
+
+
+
+
+
+namespace interfacedemo{
+ 
+
+
+public class Shape{
+
+    public static void Main(){
+
+        Drawable d;
+        SetDrawable sd;
+        
+        d=new Circle();
+        d.draw();
+
+        sd=new Circle();
+        sd.SetColour();
+       
+        d=new Square();
+        d.draw();
+        d=new Rectangle();
+        d.draw();
+        d=new Line();
+        d.draw();
+        Console.ReadLine();
+        Line l=new Line();
+        l.draw();
+
+
+    }
+}
+
+
+}
